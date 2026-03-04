58fc9434b8f019df3d28371fad6448e77040be38
Ahmed | Computer Science Student <ahmughal1350@gmail.com>
2026-03-04 16:15:44 +0500

    Improve calculator: added loop and exit option

--- calculator.cpp
+++ calculator.cpp
@@ -4,36 +4,46 @@ using namespace std;
 int main() {
     double num1, num2;
     char operation;
+    char choice;
 
-    cout << "Simple C++ Calculator" << endl;
-    cout << "Enter first number: ";
-    cin >> num1;
+    cout << "===== Simple C++ Calculator =====" << endl;
 
-    cout << "Enter operator (+, -, *, /): ";
-    cin >> operation;
+    do {
+        cout << "\nEnter first number: ";
+        cin >> num1;
 
-    cout << "Enter second number: ";
-    cin >> num2;
+        cout << "Enter operator (+, -, *, /): ";
+        cin >> operation;
 
-    switch(operation) {
-        case '+':
-            cout << "Result: " << num1 + num2;
-            break;
-        case '-':
-            cout << "Result: " << num1 - num2;
-            break;
-        case '*':
-            cout << "Result: " << num1 * num2;
-            break;
-        case '/':
-            if(num2 != 0)
-                cout << "Result: " << num1 / num2;
-            else
-                cout << "Error! Division by zero.";
-            break;
-        default:
-            cout << "Invalid operator!";
-    }
+        cout << "Enter second number: ";
+        cin >> num2;
+
+        switch(operation) {
+            case '+':
+                cout << "Result: " << num1 + num2 << endl;
+                break;
+            case '-':
+                cout << "Result: " << num1 - num2 << endl;
+                break;
+            case '*':
+                cout << "Result: " << num1 * num2 << endl;
+                break;
+            case '/':
+                if(num2 != 0)
+                    cout << "Result: " << num1 / num2 << endl;
+                else
+                    cout << "Error! Division by zero." << endl;
+                break;
+            default:
+                cout << "Invalid operator!" << endl;
+        }
+
+        cout << "\nDo you want to calculate again? (y/n): ";
+        cin >> choice;
+
+    } while(choice == 'y' || choice == 'Y');
+
+    cout << "\nThank you for using the calculator!" << endl;
 
     return 0;
 }
