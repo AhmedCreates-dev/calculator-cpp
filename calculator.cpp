e7b923ae6664fdd36d19f148dc116ef9ef00fbb0
Ahmed | Computer Science Student <ahmughal1350@gmail.com>
2026-03-04 16:10:23 +0500

    Create calculator.cpp
    basic calculator in c++

---
+++ calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "Simple C++ Calculator" << endl;
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch(operation) {
+        case '+':
+            cout << "Result: " << num1 + num2;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0)
+                cout << "Result: " << num1 / num2;
+            else
+                cout << "Error! Division by zero.";
+            break;
+        default:
+            cout << "Invalid operator!";
+    }
+
+    return 0;
+}
